a8c51bb868f7f3d5e0da796f32c97fe5d4784ada
Ruslan <bagless.dev@gmail.com>
2026-04-22 01:00:02 +0300

    agent: 012-request-duration-logging (#14)
    Manifest: 012-request-duration-logging
    Run:      20260421T212018Z-012-request-duration-logging
    
    Co-authored-by: agent-poc <agent@baglessdev>

--- middleware.go
+++ middleware.go
@@ -3,7 +3,9 @@ package main
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"log"
 	"net/http"
+	"time"
 )
 
 func withRequestID(next http.Handler) http.Handler {
@@ -21,4 +23,32 @@ func generateRequestID() string {
 	b := make([]byte, 8)
 	_, _ = rand.Read(b)
 	return hex.EncodeToString(b)
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func withLogging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		lrw := &loggingResponseWriter{
+			ResponseWriter: w,
+			statusCode:     200,
+		}
+
+		next.ServeHTTP(lrw, r)
+
+		duration := time.Since(start)
+		durationMs := int(duration.Milliseconds())
+		requestID := lrw.Header().Get("X-Request-Id")
+
+		log.Printf("%s %s %d %dms %s", r.Method, r.URL.Path, lrw.statusCode, durationMs, requestID)
+	})
 }
